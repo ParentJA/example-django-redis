c22eb07ff58e232ab57241183bad9a49f01d2eb6
Jason Parent <ParentJ@advisory.com>
2016-07-28 23:02:12 -0400

    Added both functions, one with cache and one without.

--- cookbook/services.py
+++ cookbook/services.py
@@ -1,13 +1,33 @@
 from django.core.cache import cache
+from django.db import reset_queries, connection
 from .models import Recipe
 
 
-def get_recipes():
+def get_recipes_without_cache():
+	# Reset database queries.
+    reset_queries()
+
+    # Retrieve recipes.
+    recipes = list(Recipe.objects.prefetch_related('ingredient_set__food'))
+
+    # Count database queries.
+    print 'Database called {count} times.'.format(count=len(connection.queries))
+
+    return recipes
+
+
+def get_recipes_with_cache():
+    # Reset database queries.
+    reset_queries()
+
+    # Retrieve recipes.
     if 'recipes' in cache:
         recipes = cache.get('recipes')
     else:
         recipes = list(Recipe.objects.prefetch_related('ingredient_set__food'))
         cache.set('recipes', recipes)
 
-    return recipes
+    # Count database queries.
+    print 'Database called {count} times.'.format(count=len(connection.queries))
 
+    return recipes
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .services import get_recipes
+from .services import get_recipes_with_cache as get_recipes
 
 
 def recipes_view(request):
