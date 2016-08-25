31562a4b7b345d5b5241bf44471e5d2aa1836b55
Jason Parent <ParentJ@advisory.com>
2016-08-25 00:11:00 -0400

    Removed database tracking code.

--- cookbook/services.py
+++ cookbook/services.py
@@ -1,33 +1,19 @@
+from django.conf import settings
 from django.core.cache import cache
-from django.db import reset_queries, connection
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from .models import Recipe
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 def get_recipes_without_cache():
-    # Reset database queries.
-    reset_queries()
-
-    # Retrieve recipes.
-    recipes = list(Recipe.objects.prefetch_related('ingredient_set__food'))
-
-    # Count database queries.
-    print 'Database called {count} times.'.format(count=len(connection.queries))
-
-    return recipes
+    return list(Recipe.objects.prefetch_related('ingredient_set__food'))
 
 
 def get_recipes_with_cache():
-    # Reset database queries.
-    reset_queries()
-
-    # Retrieve recipes.
     if 'recipes' in cache:
         recipes = cache.get('recipes')
     else:
         recipes = list(Recipe.objects.prefetch_related('ingredient_set__food'))
-        cache.set('recipes', recipes)
-
-    # Count database queries.
-    print 'Database called {count} times.'.format(count=len(connection.queries))
-
+        cache.set('recipes', recipes, timeout=CACHE_TTL)
     return recipes
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from .services import get_recipes_without_cache as get_recipes
 
-# Cache time to live is 15 minutes.
-CACHE_TTL = 60 * 15
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 @cache_page(CACHE_TTL)
